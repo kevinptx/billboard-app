015b56848d76a01059dd925e6a33e5836e676cf5
Kevin Peery <kevin.peery@gmail.com>
2018-11-18 22:03:31 -0700

    Add and edit views. Add materialize.

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
-gem "semantic-ui-sass"
+gem 'materialize-sass', '~> 1.0.0'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,54 @@
 class SongsController < ApplicationController
+  before_action :set_song
+  before_action :set_board, except: [:index, :new, :create]
+
   def index
+    @songs = @board.songs
   end
 
   def show
   end
 
   def new
+    @song = @board.songs.new
+  end
+
+  def create
+    @song = @board.songs.new(song_params)
+    if @song.save
+      redirect_to board_song_path
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to board_songs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to board_songs_path
+  end
+
+  private
+
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+
+  def set_song
+    @song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name)
   end
 end
